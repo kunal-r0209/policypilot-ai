7ebaa44bde8b49d122c81a487ae9dd8565bf9c1e
SprintProject <kl07kunal@gmail.cc0m>
2026-06-13 12:58:48 +0530

    deploy ai insurance bot

--- app.py
+++ app.py
@@ -106,6 +106,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000)),
+      port=int(os.environ.get("PORT", 7860)),
         workers=1
     )
